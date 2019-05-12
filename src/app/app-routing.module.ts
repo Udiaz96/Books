455a53d43d9bef4db50b17b2545e70cd2b280ccb
Uriel Díaz Villamil <urieldiazvillamil@MBP-de-Uriel.lan>
2019-05-12 12:33:07 -0500

    Creando autenticacinnn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
+
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
   {path: 'login', component: LoginComponent}
 ];
+
+
+//const routes: Routes = []
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    user: new FormControl(''),
+    password: new FormControl('')
+  });
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit()
+  {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
   }
 
 }
